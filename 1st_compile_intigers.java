0a479b60ec09af66e1b87f0014e5c41688f82b12
mohith <16695a0313@gmail.com>
2022-10-03 12:44:11 +0530

    first_commit

---
+++ 1st_compile_intigers.java
@@ -0,0 +1,15 @@
+class Main {
+
+  public static void main(String[] args) {
+    
+    System.out.println("Enter two numbers");
+    int first = 30;
+    int second = 80;
+    
+    System.out.println(first + " " + second);
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println("The sum is: " + sum);
+  }
+}
